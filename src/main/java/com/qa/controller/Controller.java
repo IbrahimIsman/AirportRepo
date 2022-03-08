877b590ab45b9b53278d65a5077a83ae9ca47b68
Your Name <you@example.com>
2022-03-08 12:24:26 +0000

    set up entity, service, repo and controller packages. Intoduced add to airport functionality

--- src/main/java/com/qa/controller/Controller.java
+++ src/main/java/com/qa/controller/Controller.java
@@ -1,5 +1,29 @@
 package com.qa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.entity.Airport;
+import com.qa.service.AirportService;
+
+@RestController
 public class Controller {
 
+	private AirportService airport;
+
+	@Autowired
+	public Controller(AirportService airport) {
+		super();
+		this.airport = airport;
+	}
+	
+	@PostMapping("/addToAirport")
+	public ResponseEntity<Airport> addToAirport (@RequestBody Airport aeroplane){
+	   return new ResponseEntity<Airport> (this.airport.addAeroplane(aeroplane), HttpStatus.CREATED);
+	}
+	
 }
--- src/main/java/com/qa/entity/Airport.java
+++ src/main/java/com/qa/entity/Airport.java
@@ -1,5 +1,70 @@
 package com.qa.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Airport {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	String type;
+	int capacity;
+	float price;
+	
+	public Airport () {}
+	
+	public Airport(String type, int capacity, float price) {
+		super();
+		this.type = type;
+		this.capacity = capacity;
+		this.price = price;
+	}
+	
+    public Airport(int id, String type, int capacity, float price) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.capacity = capacity;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+	
+	
 }
